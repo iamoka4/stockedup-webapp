415352aef5eea9bece55674ce2092c77c79634d8
iamoka4 <henrysmart501@gmail.com>
2026-08-27 07:55:31 +0100

    Add rewrites for backend API requests

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["10.55.119.198"],
+  async rewrites() {
+    return [
+      {
+        source: "/backend/api/:path*",
+        destination: "https://api.stockedup.africa/backend/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
